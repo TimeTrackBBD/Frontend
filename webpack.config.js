7df5b3540b7f9c722dde667d8337de9150505f14
Liam Talberg <liamtalberg@gmail.com>
2024-06-02 21:32:48 +0200

    Enable webpack hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,38 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const path = require('path');
+const path = require("path");
+
 const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html", 
-  filename: "./index.html"
+  template: "./src/index.html",
+  filename: "./index.html",
 });
+
 module.exports = {
   entry: "./src/index.js",
-  output: { // NEW
-    path: path.join(__dirname, 'dist'),
-    filename: "[name].js"
-  }, // NEW Ends
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+    publicPath: "/",
+  },
   plugins: [htmlPlugin],
   module: {
     rules: [
-        {
-            test: /\.(js|jsx)$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: 'babel-loader',
-            options: { presets: ['@babel/env','@babel/preset-react'] },
-          },
-          {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-          },
-    ]
-  }
-
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env", "@babel/preset-react"] },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    hot: true,
+    historyApiFallback: true,
+  },
 };
