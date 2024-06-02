e43098c2e7b3d30c757be71005a311c6b16d466c
Liam Talberg <liamtalberg@gmail.com>
2024-06-02 11:23:29 +0200

    Deploy site to env

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,50 +1,24 @@
 provider "aws" {
-  region = "eu-west-1" # Specify your region
+  region = "eu-west-1"
 }
 
-resource "aws_elastic_beanstalk_application" "time_track_app" {
-  name        = "TimeTrack"
-  description = "Elastic Beanstalk Application for TimeTrack"
+data "aws_elastic_beanstalk_environment" "existing_time_track_env" {
+  name = "TimeTrack-env" 
 }
 
-resource "aws_elastic_beanstalk_environment" "time_track_env" {
-  name                = "TimeTrackEnvironment"
-  application         = aws_elastic_beanstalk_application.time_track_app.name
-  solution_stack_name = "64bit Amazon Linux 2 v5.9.2 running Node.js 14"
-
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "IamInstanceProfile"
-    value     = aws_iam_instance_profile.eb_instance_profile.name
+resource "aws_s3_bucket" "time_track_bucket" {
+  bucket = "time-track-app-bucket"
+  acl    = "private"
+  
+  tags = {
+    Name = "time-track-app-bucket"
   }
 }
 
-resource "aws_iam_role" "eb_instance_role" {
-  name = "eb-instance-role"
-
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
-}
-
-resource "aws_iam_instance_profile" "eb_instance_profile" {
-  name = "eb-instance-profile"
-  role = aws_iam_role.eb_instance_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "eb_instance_role_policy" {
-  role       = aws_iam_role.eb_instance_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
-}
-
-resource "aws_iam_role_policy_attachment" "eb_instance_role_s3" {
-  role       = aws_iam_role.eb_instance_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+resource "aws_elastic_beanstalk_application_version" "time_track_app_version" {
+  name          = "TimeTrackApp"
+  application   = data.aws_elastic_beanstalk_environment.existing_time_track_env.application
+  description   = "The TimeTrack application"
+  bucket        = aws_s3_bucket.time_track_bucket.id
+  key           = "build.zip"
 }
