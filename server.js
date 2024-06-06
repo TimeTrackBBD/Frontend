4cf2674b615e2326db566f0ee1cc46d9d9efc051
Cameron Worthington <cameron.worthington@bbd.co.za>
2024-06-06 10:58:27 +0200

    removed can't find errors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const DIST_DIR = path.join(__dirname, '/dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html'); 
 
 app.use(express.static(DIST_DIR)); 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
  res.sendFile(HTML_FILE);
