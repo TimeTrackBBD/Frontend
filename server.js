70863b0963153d8eff1b9fde9c038c771463d33d
Cameron Worthington <cameron.worthington@bbd.co.za>
2024-06-06 14:57:51 +0200

    attempt 2 to serve manifest and ico

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const DIST_DIR = path.join(__dirname, '/dist');
+const PUBLIC_DIR = path.join(__dirname, '/public');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
-const FAV_ICON = path.join(DIST_DIR, '/public/favicon.ico');
-const MANIFEST = path.join(DIST_DIR, '/public/manifest.json');
+const FAV_ICON = path.join(PUBLIC_DIR, 'favicon.ico');
+const MANIFEST = path.join(PUBLIC_DIR, 'manifest.json');
 app.use(express.static(DIST_DIR));
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(PUBLIC_DIR));
 
 app.get('/', (req, res) => {
     res.sendFile(HTML_FILE);
